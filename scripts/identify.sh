8ca6f469f81873ec91f16c937950e94c7c6041e8
IGBC <fakespamtrap@gmail.com>
2017-08-18 18:38:43 +0200

    added new identify script and halfscreen hack (for acer 3440x1440 monitors that support PBP, this script forces the output to 1720x1440, exactly one horisontal half of the display alowing the PC to maintain 1:1 scaling when the PBP functionality of the monitor is used. Great when combined with a PS3 playing a blu ray on the other half.)

--- scripts/identify.sh
+++ scripts/identify.sh
@@ -1,1 +1,17 @@
-cowsay "Host: $(hostname) --------------------------------------- $(uptime) --------------------------------------- $(ifconfig | grep inet | grep -v inet6)"
+host="Host: $(hostname)"
+up=$(uptime | sed -e 's/^[[:space:]]*//')
+netline=$(ip a | grep inet | sed -e 's/^[[:space:]]*//' | cut -d " " -f-2 | sort)
+net=$"$(for i in $netline; do echo ${i}; done)"
+
+
+if command -v cowsay >/dev/null; then 
+	output=$(cowsay "${host} --------------------------------------- ${up} --------------------------------------- ${net}")
+else
+	output=$(echo "${host} -- ${up}";echo "Addresses:"; echo "${netline}")
+fi
+
+if command -v lolcat >/dev/null; then
+	lolcat "${output}"
+else
+	echo "${output}"
+fi
